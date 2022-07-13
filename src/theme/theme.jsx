ccfcef85752e4dd31569aaa6b212ed9536dd0548
Diego Lima <xxdiegolsxx@gmail.com>
2022-07-13 16:30:53 -0300

    theme

---
+++ src/theme/theme.jsx
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    primaryColor: '#313235',
+    secondaryColor: '#00A3FF',
+    thirdColor: '#FFFEFD',
+  },
+  sizes: {
+    size1: '4.8rem',
+    size2: '4rem',
+    size3: '1.8rem',
+    size4: '1.4rem',
+  },
+};
