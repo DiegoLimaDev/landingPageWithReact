05a86241879caeb53eb8ec32ec8ad17f11c7548b
Diego Lima <xxdiegolsxx@gmail.com>
2022-07-20 21:32:23 -0300

    home menu to phones

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { theme } from './theme/theme';
 import { Link as ScrollTarget } from 'react-scroll';
 
+import { Navicon } from '@styled-icons/evil';
+import { Close } from '@styled-icons/evil';
 import logo from './assets/images/logo_crd_data.png';
 import AboutUs from './pages/AboutUs';
 import Partners from './pages/Partners';
@@ -16,24 +18,25 @@ const Container = styled.div`
 `;
 
 const NavBar = styled.div`
-  display: flex;
-  flex-direction: row;
-  height: 10%;
-  width: 100%;
-  background-color: ${theme.colors.primaryColor};
-  align-items: center;
-  margin: 0 auto;
-  position: sticky;
-  top: 0;
-  z-index: 1;
+  ${({ visible }) => css`
+    display: flex;
+    flex-direction: row;
+    height: 10%;
+    width: 100%;
+    background-color: ${theme.colors.primaryColor};
+    align-items: center;
+    margin: 0 auto;
+    position: sticky;
+    top: 0;
+    z-index: 1;
 
-  @media ${theme.medias.media1} {
-    visibility: hidden;
-    position: relative;
-    flex-direction: column;
-    height: 0;
-    width: 0;
-  }
+    @media ${theme.medias.media1} {
+      /* position: block; */
+      flex-direction: column;
+      ${visible ? MenuVisible() : MenuInvisible()}
+      margin: 0;
+    }
+  `}
 `;
 
 const Links = styled.p`
@@ -56,6 +59,12 @@ const Links = styled.p`
     bottom: -15%;
     left: 50%;
     position: absolute;
+
+    @media (max-width: 500px) {
+      visibility: hidden;
+      height: 0;
+      width: 0;
+    }
   }
 
   &:hover::after {
@@ -63,12 +72,6 @@ const Links = styled.p`
     left: 0;
     position: absolute;
     transition: all 200ms ease-in-out;
-  }
-
-  @media ${theme.medias.media1} {
-    width: 0;
-    height: 0;
-    visibility: collapse;
   }
 `;
 
@@ -79,43 +82,106 @@ const Dropdown = styled.select`
   border: 0;
   margin-left: auto;
   margin-right: 2%;
+
+  @media ${theme.medias.media1} {
+    display: block;
+    margin: 0 auto;
+  }
 `;
 
-const SwitchLanguage = styled.div`
+// const SwitchLanguage = styled.div`
+//   visibility: hidden;
+//   height: 0;
+
+//   @media ${theme.medias.media1} {
+//     visibility: visible;
+//     display: block;
+//     position: absolute;
+//     top: 20px;
+//     right: 50%;
+//     left: 50%;
+//   }
+// `;
+
+const MenuVisible = () => css`
+  visibility: visible;
+  opacity: 1;
+  height: auto;
+`;
+
+const MenuInvisible = () => css`
+  visibility: hidden;
+  opacity: 1;
+  height: 0;
+`;
+
+const Menu = styled.div`
   visibility: hidden;
   height: 0;
 
   @media ${theme.medias.media1} {
     visibility: visible;
-    display: block;
-    position: absolute;
-    top: 20px;
-    right: 50%;
-    left: 50%;
+    display: flex;
+    align-items: center;
+    position: sticky;
+    top: 0;
+    background-color: ${theme.colors.primaryColor};
+    height: auto;
+    direction: ltr;
   }
 `;
 
 const Home = () => {
   const [language, setLanguage] = useState('PT');
+  const [visible, setVisible] = useState(false);
 
   return (
     <Container>
-      <SwitchLanguage>
+      {visible ? null : (
+        <Menu>
+          <Navicon
+            size={50}
+            color={theme.colors.thirdColor}
+            onClick={() => setVisible(true)}
+          />
+          <img src={logo} style={{ display: 'block', margin: '0 auto' }} />
+        </Menu>
+      )}
+      {/* <SwitchLanguage>
         <Dropdown onChange={() => setLanguage(language === 'PT' ? 'EN' : 'PT')}>
           <option value="PT">PT</option>
           <option value="EN">EN</option>
         </Dropdown>
-      </SwitchLanguage>
-
-      <NavBar>
-        <img src={logo} style={{ marginLeft: '2rem' }} />
+      </SwitchLanguage> */}
+      <NavBar visible={visible}>
+        {visible ? null : <img src={logo} style={{ marginLeft: '2rem' }} />}
+        <>
+          {visible ? (
+            <div
+              style={{
+                display: 'flex',
+                direction: 'ltr',
+                alignItems: 'center',
+                width: '100%',
+              }}
+            >
+              <Close
+                size={50}
+                color={theme.colors.thirdColor}
+                onClick={() => setVisible(false)}
+              />
+              <img src={logo} style={{ display: 'block', margin: '0 auto' }} />
+            </div>
+          ) : null}
+        </>
         <Links>
           <ScrollTarget
             to="aboutUs"
             spy={true}
             smooth={true}
             duration={500}
-            offset={-50}
+            offset={visible ? -250 : -50}
+            onClick={() => setVisible(false)}
           >
             {language === 'PT' ? 'Sobre' : 'About us'}
           </ScrollTarget>
@@ -126,7 +192,8 @@ const Home = () => {
             spy={true}
             smooth={true}
             duration={500}
-            offset={-150}
+            offset={visible ? -600 : -150}
+            onClick={() => setVisible(false)}
           >
             {language === 'PT' ? 'Parceiros' : 'Partners'}
           </ScrollTarget>
@@ -137,13 +204,20 @@ const Home = () => {
             spy={true}
             smooth={true}
             duration={500}
-            offset={-100}
+            offset={visible ? -400 : -100}
+            onClick={() => setVisible(false)}
           >
             {language === 'PT' ? 'Serviços' : 'Services'}
           </ScrollTarget>
         </Links>
         <Links>
-          <ScrollTarget to="talkToUs" spy={true} smooth={true} duration={500}>
+          <ScrollTarget
+            to="talkToUs"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={() => setVisible(false)}
+          >
             {language === 'PT' ? 'Fale conosco' : 'Contact'}
           </ScrollTarget>
         </Links>
