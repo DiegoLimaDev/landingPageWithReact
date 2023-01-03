2e0861a0e5ff70563fe46e0179301fa06f9fa963
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-03 14:53:44 -0300

    build: private false

--- src/index.jsx
+++ src/index.jsx
@@ -7,9 +7,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="base">
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
