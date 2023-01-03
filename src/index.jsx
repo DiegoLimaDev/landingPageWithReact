a1d734f95d06d28f2eb7d3c5eda32b20adf4b319
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-03 14:59:22 -0300

    build: basename

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname || ''}>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
